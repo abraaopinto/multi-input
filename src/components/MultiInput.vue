5739431c9a02c4acbee10cebd92420820523c102
Abraão Pinto <pxzabraao13@yahoo.com.br>
2019-10-01 21:30:11 -0300

    aplicando algumas regras e limite de caracteres.

--- src/components/MultiInput.vue
+++ src/components/MultiInput.vue
@@ -2,8 +2,11 @@
     <div id="multi-input">
         <h1>Componente MultiInput Mask</h1>
         <div>
-            <input type="text" v-model="inputValue" placeholder="Informe o CPF, CNPJ, Raiz CNPJ ou Nome/Razão Social." size="80" min="3">
+            <input id="multi-input" type="text" v-model="inputValue" placeholder="Informe o CPF, CNPJ, Raiz CNPJ ou Nome/Razão Social." size="80" minlength="3" v-bind:maxlength="limiteMaximoCampo">
             <button v-bind="validateInput()">Pesquisar</button>
+            <div v-if="pesquisarPorRaizCnpj === true">
+                <input type="checkbox" name="raizCnpj"> Deseja pesquisar pela raiz do CNPJ.<br>
+            </div>
         </div>
         <div>
             <ul>
@@ -13,6 +16,11 @@
                 <li>CNPJ: {{ cnpj }} </li>
                 <li>Raiz CNPJ: {{ cnpjRaiz }} </li>
                 <li>Nome ou Razao Social: {{ nomeRazaoSocial }} </li>
+            </ul>
+        </div>
+        <div class="regras">
+            <ul>
+                <li>Regra 1 - Analisa se existe pelo menos uma letra [a-z]. Retorna o valor na propriedade nomeRazaoSocial.</li>
             </ul>
         </div>
     </div>
@@ -27,22 +35,55 @@ export default {
              cpf: '',
              cnpj: '',
              cnpjRaiz: '',
-             nomeRazaoSocial: ''
+             nomeRazaoSocial: '',
+             pesquisarPorRaizCnpj: false,
+             limiteMaximoCampo: 100,
          }
      },
-     methods: {
+    methods: {
          validateInput: function (){
-            console.log(this.inputValue.search(/[a-z]/i));
-             if ( this.inputValue.trim != '' )
-                if(this.inputValue.length > 5 && this.inputValue.length <= 8)
-                    this.cnpjRaiz = this.inputValue;      
-                if( this.inputValue.search(/[a-z]/i) === this.inputValue.length)
-                    this.nomeRazaoSocial = this.inputValue; 
-                else
-                    this.nomeRazaoSocial = '';
-         }
-
-     }
+            
+            const regexRegra1 = /[A-Zi]/i;
+            this.inputValue = this.inputValue;
+            //console.log(this.inputValue.match(regexRegra1));
+            // Regra 1 - somente nomeRazaoSocial recebe o valor de inputValue pois o campo contem letras e numeros.
+            if(this.inputValue.match(regexRegra1) !== null){
+                this.limiteMaximoCampo = 100;
+                this.nomeRazaoSocial = this.inputValue;
+                this.idFiscal = '';
+                this.cpf = '';
+                this.cnpj = '';
+                this.cnpjRaiz = '';
+            }else{
+                // Reseta o valor de nomeRazaoSocial.
+                this.nomeRazaoSocial = '';
+                this.limiteMaximoCampo = 14;
+                // Realizar as validações para os demais variaveis.
+                if(this.inputValue.length <= 14 && this.inputValue.length >= 5){
+                    this.idFiscal = this.inputValue; 
+                }else{
+                    this.idFiscal = '';
+                }
+                if(this.inputValue.length <= 11 && this.inputValue.length >= 5){
+                    this.cpf = this.inputValue; 
+                }else{
+                    this.cpf = '';
+                }
+                if(this.inputValue.length <= 14 && this.inputValue.length >= 8){
+                    this.cnpj = this.inputValue; 
+                }else{
+                    this.cnpj = '';
+                }
+                if(this.inputValue.length <= 8 && this.inputValue.length >= 5){
+                    this.pesquisarPorRaizCnpj = true;
+                    this.cnpjRaiz = this.inputValue;
+                }else{
+                    this.pesquisarPorRaizCnpj = false;
+                    this.cnpjRaiz = '';
+                }                
+            }                         
+        }
+    }
 }
 </script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
