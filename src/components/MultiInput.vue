70d4ac5a011a3515ca39daadfa846ff66953d883
Abraão Pinto <pxzabraao13@yahoo.com.br>
2019-10-03 18:09:08 -0300

    regra de validação de cpf e cnpj versao 1

--- src/components/MultiInput.vue
+++ src/components/MultiInput.vue
@@ -4,18 +4,19 @@
         <div class="center">
             <input id="multi-input" type="text" v-model="inputValue" placeholder="Informe o CPF, CNPJ, Raiz CNPJ ou Nome/Razão Social." size="80" minlength="3" v-bind:maxlength="limiteMaximoCampo">
             <button v-bind="validateInput()">Pesquisar</button>
-            <div v-if="pesquisarPorRaizCnpj === true">
-                <input type="checkbox" name="raizCnpj"> Deseja pesquisar pela raiz do CNPJ.<br>
+            <div v-if="inputValue.length === 8 ">
+                <input type="checkbox" v-bind:value="pesquisarPorRaizCnpj" v-on:input="pesquisarPorRaizCnpj = $event.target.value"> Deseja pesquisar pela raiz do CNPJ.<br>
             </div>
         </div>
         <div>
             <ul>
-                <li>InputValue: {{ inputValue }} </li>
                 <li>IdFiscal: {{ idFiscal }} </li>
                 <li>CPF: {{ cpf }} </li>
                 <li>CNPJ: {{ cnpj }} </li>
                 <li>Raiz CNPJ: {{ cnpjRaiz }} </li>
                 <li>Nome ou Razao Social: {{ nomeRazaoSocial }} </li>
+                <li>Quantidade de caracteres digitados: {{ inputValue.length }}</li>
+                <li>Deseja pesquisar pela raiz do CNPJ: {{ pesquisarPorRaizCnpj }}</li>
             </ul>
         </div>
         <div class="regras">
@@ -44,10 +45,8 @@ export default {
          }
      },
     methods: {
-         validateInput: function (){
-            
+        validateInput(){
             const regexRegra1 = /[A-Zi]/i;
-            this.inputValue = this.inputValue;
             
             // Regra 1 - somente nomeRazaoSocial recebe o valor de inputValue pois o campo contem letras e numeros.
             if(this.inputValue.match(regexRegra1) !== null){
@@ -58,33 +57,128 @@ export default {
                 this.cnpj = '';
                 this.cnpjRaiz = '';
             }else{
+                /*  Analise dos demais casos onde todos os digitos são numericos. */
                 // Reseta o valor de nomeRazaoSocial.
                 this.nomeRazaoSocial = '';
+                
                 this.limiteMaximoCampo = 14;
                 // Realizar as validações para os demais variaveis.
-                if(this.inputValue.length <= 14 && this.inputValue.length >= 5){
+
+                // Verifica se é um IdFiscal
+                if(this.validarCPF(this.inputValue.padStart(11,'0')) || this.validarCNPJ(this.inputValue.padStart(14,'0'))){
                     this.idFiscal = this.inputValue; 
                 }else{
                     this.idFiscal = '';
                 }
-                if(this.inputValue.length <= 11 && this.inputValue.length >= 5){
+
+                // Verifica se é um CPF
+                if(this.validarCPF(this.inputValue.padStart(11,'0'))){
                     this.cpf = this.inputValue; 
+                    this.limiteMaximoCampo = 11;
                 }else{
                     this.cpf = '';
                 }
-                if(this.inputValue.length <= 14 && this.inputValue.length >= 8){
+
+                // Verifica se é um CNPJ
+                if(this.validarCNPJ(this.inputValue.padStart(14,'0'))){
+                    this.limiteMaximoCampo = 14;
                     this.cnpj = this.inputValue; 
                 }else{
                     this.cnpj = '';
                 }
-                if(this.inputValue.length <= 8 && this.inputValue.length >= 5){
-                    this.pesquisarPorRaizCnpj = true;
-                    this.cnpjRaiz = this.inputValue;
-                }else{
-                    this.pesquisarPorRaizCnpj = false;
-                    this.cnpjRaiz = '';
-                }                
+
+                // Verifica se é uma Raiz do CNPJ.
+                if(this.inputValue.length <= 8 && this.pesquisarPorRaizCnpj ) this.cnpjRaiz = this.inputValue;
+                
+                // Verifica se o usuario não deseja consultar pela Raiz do CNPJ.
+                if(this.inputValue.length <= 8 && !this.pesquisarPorRaizCnpj ) this.cnpjRaiz = '';  
             }                         
+        },
+        validarCPF(pCpf){
+                       
+            if ( pCpf.length != 11 ||
+            pCpf == null || 
+            pCpf ==  "" ||
+            pCpf == undefined ||
+            pCpf == "00000000000" ||
+            pCpf == "11111111111" ||
+            pCpf == "22222222222" ||
+            pCpf == "33333333333" ||
+            pCpf == "44444444444" ||
+            pCpf == "55555555555" ||
+            pCpf == "66666666666" ||
+            pCpf == "77777777777" ||
+            pCpf == "88888888888" ||
+            pCpf == "99999999999" ) return false;
+
+            var Soma;
+            var Resto;
+            Soma = 0;
+            
+            for (var i=1; i<=9; i++) Soma = Soma + parseInt(pCpf.substring(i-1, i)) * (11 - i);
+            
+            Resto = (Soma * 10) % 11;
+            
+            if ((Resto == 10) || (Resto == 11))  Resto = 0;
+            
+            if (Resto != parseInt(pCpf.substring(9, 10)) ) return false;
+            
+            Soma = 0;
+            
+            for (var j = 1; j <= 10; j++) Soma = Soma + parseInt(pCpf.substring(j-1, j)) * (12 - j);
+                
+            Resto = (Soma * 10) % 11;
+
+            if ((Resto == 10) || (Resto == 11))  Resto = 0;
+            
+            if (Resto != parseInt(pCpf.substring(10, 11) ) ) return false;
+            
+            return true;
+        },
+        validarCNPJ(pCnpj) {
+            pCnpj = pCnpj.replace(/[^\d]+/g,'');
+        
+            // Elimina CNPJs invalidos conhecidos
+            if ( pCnpj.length != 14 ||
+            pCnpj == null || 
+            pCnpj ==  "" || 
+            pCnpj == undefined || 
+            pCnpj == "00000000000000" || 
+            pCnpj == "11111111111111" || 
+            pCnpj == "22222222222222" || 
+            pCnpj == "33333333333333" || 
+            pCnpj == "44444444444444" || 
+            pCnpj == "55555555555555" || 
+            pCnpj == "66666666666666" || 
+            pCnpj == "77777777777777" || 
+            pCnpj == "88888888888888" || 
+            pCnpj == "99999999999999") return false;
+            
+            // Valida DVs
+            var tamanho = pCnpj.length - 2;
+            var numeros = pCnpj.substring(0,tamanho);
+            var digitos = pCnpj.substring(tamanho);
+            var soma = 0;
+            var pos = tamanho - 7;
+            for (i = tamanho; i >= 1; i--) {
+                soma += numeros.charAt(tamanho - i) * pos--;
+                if (pos < 2) pos = 9;
+            }
+            var resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+            if (resultado != digitos.charAt(0)) return false;
+            tamanho = tamanho + 1;
+            numeros = pCnpj.substring(0,tamanho);
+            soma = 0;
+            pos = tamanho - 7;
+            
+            for (var i = tamanho; i >= 1; i--) {
+                soma += numeros.charAt(tamanho - i) * pos--;
+                if (pos < 2) pos = 9;
+            }
+            resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+            
+            if (resultado != digitos.charAt(1)) return false;
+            return true;
         }
     }
 }
